7168a27918941ec69a56f242d763dc624f80f1f7
Nandziro <kolkovgleb@gmail.com>
2025-11-28 21:51:25 +0100

    Fix

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import './styles/vars.css';
 
 import App from './components/App/App.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
